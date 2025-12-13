38f25b4726627a8ff8617a39ae39c7f207d27f57
Elias Lopez Fematt <210356@edu.uaa.mx>
2025-12-12 18:52:03 -0600

    Sin unfold

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,6 +1,35 @@
 from django.contrib import admin
-from tickets.models import Ticket
-# Register your models here.
+from django.db.models import Sum
+from tickets.models import Alumno, Pago
 
+class PagoInline(admin.TabularInline):
+    model = Pago
+    extra = 0
 
-admin.site.register(Ticket)
+class AlumnoAdmin(admin.ModelAdmin):
+    inlines = [PagoInline]
+    list_display = ('nombre', 'estado', 'invitados_min', 'invitados_max', 'boletos_pagados')
+    list_filter = ('estado',)
+
+    def get_readonly_fields(self, request, obj=None):
+        if not request.user.is_superuser:
+            return ('nombre', 'invitados_min', 'invitados_max') 
+        return super().get_readonly_fields(request, obj)
+
+    def get_list_editable(self, request):
+        if request.user.is_superuser:
+            return ('estado',)
+        return ('estado',)
+
+    def changelist_view(self, request, extra_context=None):
+        self.list_editable = self.get_list_editable(request)
+        return super().changelist_view(request, extra_context)
+
+    def boletos_pagados(self, obj):
+        total_boletos = obj.pagos.aggregate(Sum('cantidad_boletos'))['cantidad_boletos__sum'] or 0
+        return total_boletos
+    
+    boletos_pagados.short_description = "Boletos Pagados"
+
+admin.site.register(Alumno, AlumnoAdmin)
+admin.site.register(Pago)
--- tickets/models.py
+++ tickets/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
-# Create your models here.
-class Ticket(models.Model):
-    nombre_alumno = models.CharField(max_length=100)
-    id_alumno = models.IntegerField(null=True, blank=True)
-    ticket = CloudinaryField(
-        "Archivo de imagen",
-        folder="tickets"
-    )
+
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=200)
+    invitados_min = models.IntegerField(default=0)
+    invitados_max = models.IntegerField(default=0)
+    
+    ESTADOS = [
+        ('pendiente', 'Pendiente de Pago'),
+        ('al_corriente', 'Al Corriente'),
+        ('prorroga', 'Prórroga'),
+    ]
+    estado = models.CharField(max_length=20, choices=ESTADOS, default='pendiente')
+
+    def __str__(self):
+        return self.nombre
+
+class Pago(models.Model):
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE, related_name='pagos')
+    comprobante = CloudinaryField("Comprobante", folder="pagos_graduacion")
+    cantidad_boletos = models.IntegerField(default=1, verbose_name="Cantidad de Boletos")
+    fecha_subida = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.alumno.nombre} - {self.cantidad_boletos} boletos"
